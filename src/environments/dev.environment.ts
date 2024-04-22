e5d0a8b5b4a18c63f5261beac6c3dc1374514647
Alessio Sarica <a.sarica@unidea.org>
2024-04-22 11:06:03 +0200

    edit db name

--- src/environments/dev.environment.ts
+++ src/environments/dev.environment.ts
@@ -4,7 +4,7 @@ export default {
     userDB: 'root',
     passwordDB: 'root',
     urlDB: '127.0.0.1',
-    databaseName: 'appdb',
+    databaseName: 'AppDB',
     origins: [
         'http://localhost:8080',
         'http://127.0.0.1:8080',
