5ca39b8e0ea010d893591f29e134ea2369c24e1c
Alessio Sarica <alessiosarica05+dev@gmail.com>
2024-05-25 18:35:58 +0200

    fix console.log and imports

--- src/controller/tools.controller.ts
+++ src/controller/tools.controller.ts
@@ -1,7 +1,6 @@
 import express from 'express';
 import { getUserBySessionToken } from '../db/authentication.db';
-import fs from 'fs';
-import { join, resolve } from 'path';
+import { resolve } from 'path';
 
 export const isLoggedController = async (req: express.Request, res: express.Response) => {
     const { sessionToken } = req.cookies;
@@ -22,8 +21,7 @@ export const isLoggedController = async (req: express.Request, res: express.Resp
 export const getImgController = async (req: express.Request, res: express.Response) => {
     const { imgID } = req.params;
     const path = resolve(__dirname, `../../public/${imgID}`)
-    console.log(path);
     res.sendFile(path, (err) => {
-        
+        throw new Error(`Error 10: ${err}`);
     });
 }
