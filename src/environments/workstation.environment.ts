892bdd7fda60080bd8ddd8fbeac3b818565d387c
Alessio S. <alessiosarica05@gmail.com>
2024-08-21 00:36:18 +0200

    Update info
    Update README, package.json and env files

--- src/environments/workstation.environment.ts
+++ src/environments/workstation.environment.ts
@@ -1,12 +1,10 @@
 export default {
-    userDB: 'root',
-    passwordDB: '',
-    urlDB: '127.0.0.1',
-    databaseName: 'AppBarDB',
-    serverPort: 8080,
-    portDB: 3306,
-    origins: [
-        'http://127.0.0.1:5500/'
-    ],
-    secretKey: '9f8hw3iurfhwiu3hfuis3',
+  userDB: "",
+  passwordDB: "",
+  urlDB: "",
+  databaseName: "",
+  serverPort: 5001,
+  portDB: 3306,
+  origins: [],
+  secretKey: "",
 };
--- src/index.ts
+++ src/index.ts
@@ -1,21 +1,25 @@
 import bodyParser from "body-parser";
 import compression from "compression";
 import cookieParser from "cookie-parser";
-import cors from 'cors';
-import express from 'express';
+import cors from "cors";
+import express from "express";
 import routers from "./routers";
 
 import environments from "./environments";
 const app = express();
 
-app.use(bodyParser.json({limit: '5mb'}));
+app.use(bodyParser.json({ limit: "5mb" }));
 app.use(cookieParser());
 app.use(compression());
-app.use(cors({
-    origin: 'http://localhost:5500',
-    credentials: true
-}));
+app.use(
+  cors({
+    origin: [...environments().origins],
+    credentials: true,
+  })
+);
 
-app.use('/api', routers());
+app.use("/api", routers());
 
-app.listen(environments().serverPort, () => console.log(`Server listen on http://localhost:${environments().serverPort}/`));
+app.listen(environments().serverPort, () =>
+  console.log(`Server listen on http://localhost:${environments().serverPort}/`)
+);
